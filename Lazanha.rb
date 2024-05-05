f655f5663e3c2e96b975001451636c5252791c01
JUNIORCONDE <rubenscondejr@uol.com.br>
2024-05-05 17:04:38 -0300

    up

---
+++ Lazanha.rb
@@ -0,0 +1,14 @@
+=beginPI = 3.14159
+NOME_EMPRESA = "Minha Empresa Inc."
+DAYS_IN_WEEK = 7
+
+puts "O valor de PI é #{PI}"
+puts "Nosso nome de empresa é #{NOME_EMPRESA}"
+puts "Há #{DAYS_IN_WEEK} dias na semana."
+=end
+
+
+
+Lasagna = Lasagna.new
+
+EXPECTED_MINUTES_IN_OVEN = 40
