7c7441bf6cb6bbc878057b45d0f24820bd3369ff
juniorconde <rubenscondejr@uol.com.br>
2024-07-03 14:37:52 -0300

    estudo

---
+++ somaarray.rb
@@ -0,0 +1,6 @@
+# todos esses desafios estão sendo estudado e realizado com ajuda co GPT, para compreensão de cada linha, comando e função
+
+# Solicitar entrada do Usuário
+puts "Digite os numeros do array, separados por espaços"
+nums = gets.chomp.split.map(&:to_i)
+puts "Array: #{nums}"
