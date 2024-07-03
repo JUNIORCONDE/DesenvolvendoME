021c6ca4cde40afdc5d27a24df58aca7416ff324
juniorconde <rubenscondejr@uol.com.br>
2024-07-02 22:20:11 -0300

    up

--- removerelemento.rb
+++ removerelemento.rb
@@ -1,17 +1,25 @@
-def remove_element(nums, val)
-  k = 0
-  nums.each do |num|
-    if nun !=val
-      nums[k] = num
-      k + 1
-    end
+# codigo sem utilização de metodo, class e definição
+# Solicitar ao usuário que insira os números para formar o array
+puts "Digite os números para formar o array, separados por espaços:"
+input = gets.chomp
+nums = input.split.map(&:to_i) # Converte a entrada do usuário em um array de inteiros
+
+# Solicitar ao usuário que insira o valor a ser removido
+puts "Digite o valor que deseja remover do array:"
+val = gets.chomp.to_i
+
+# Inicializando variáveis
+k = 0 # Inicializamos o ponteiro k, que irá contar os elementos diferentes de val
+
+# Percorremos o array com o ponteiro i
+nums.each do |num|
+  # Se o elemento atual não é igual a val, movemos ele para a posição k
+  if num != val
+    nums[k] = num
+    k += 1 # Incrementamos k
   end
-  k
 end
 
-#Exemplo de uso
-nums = [3, 2, 2, 3]
-val = 3
-k = remove_element(nums, val)
+# Exibir o número de elementos que não são iguais ao valor especificado
 puts "Número de elementos que não são iguais a #{val}: #{k}"
 puts "Array modificado: #{nums[0...k].inspect}"
