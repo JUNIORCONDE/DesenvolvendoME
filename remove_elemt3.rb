d42dee4e86ae2cd1123c52dfd7ed0912b78a1b5d
juniorconde <rubenscondejr@uol.com.br>
2024-07-02 22:47:24 -0300

    up

--- remove_elemt3.rb
+++ remove_elemt3.rb
@@ -1,3 +1,6 @@
 # Usando delete para remover um elemento específico
 
 puts "Ola, digite numero inteiros para formar nosso array, separado por espaço"
+input = gets.chomp
+nums = input.split.map(&:to_i)
+puts "array: #{nums}"
