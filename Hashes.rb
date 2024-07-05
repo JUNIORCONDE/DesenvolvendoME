38dbaf69ad8c9f12ec4091266b272715a44af811
juniorconde <rubenscondejr@uol.com.br>
2024-07-05 09:09:10 -0300

    up

---
+++ Hashes.rb
@@ -0,0 +1,15 @@
+# Criação de um hash vazio
+meu_hash = {}
+
+# Adicionando pares chave-valor
+meu_hash[:nome] = "JUNIOR CONDE"
+meu_hash[:idade] = 30
+meu_hash[:cidade] = "PAIÇANDU"
+
+# Acessando valores
+puts meu_hash[:nome]
+puts meu_hash[:idade]
+puts meu_hash[:cidade]
+
+
+puts meu_hash
