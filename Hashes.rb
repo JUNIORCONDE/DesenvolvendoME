a70bd08d7ba4d24a93f2fdfb518c2edcfb8b6fae
juniorconde <rubenscondejr@uol.com.br>
2024-07-07 19:12:49 -0300

    up

--- Hashes.rb
+++ Hashes.rb
@@ -1,15 +1,34 @@
-# Criação de um hash vazio
-meu_hash = {}
+# Criação do hash principal para armazenar múltiplos usuários
+usuarios = {}
 
-# Adicionando pares chave-valor
-meu_hash[:nome] = "JUNIOR CONDE"
-meu_hash[:idade] = 30
-meu_hash[:cidade] = "PAIÇANDU"
+  # Coletando dados do usuário
+  puts "Digite o ID do usuário:"
+  id = gets.chomp.to_i
 
-# Acessando valores
-puts meu_hash[:nome]
-puts meu_hash[:idade]
-puts meu_hash[:cidade]
+  puts "Digite o nome do usuário:"
+  usuario[:nome] = gets.chomp
 
+  puts "Digite a idade do usuário:"
+  usuario[:idade] = gets.chomp.to_i
 
-puts meu_hash
+  puts "Digite o email do usuário:"
+  usuario[:email] = gets.chomp
+
+  # Salvando o hash do usuário no hash principal, usando o ID como chave
+  usuarios[id] = usuario
+
+  # Pergunta se deseja adicionar outro usuário
+  puts "Deseja adicionar outro usuário? (s/n)"
+  resposta = gets.chomp.downcase
+  break if resposta != 's'
+end
+
+# Exibindo todos os usuários
+puts "\nUsuários cadastrados:"
+usuarios.each do |id, dados|
+  puts "ID: #{id}"
+  dados.each do |chave, valor|
+    puts "#{chave.capitalize}: #{valor}"
+  end
+  puts "-------------------"
+end
