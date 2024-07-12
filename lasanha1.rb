e806099b414f2891a7aa026c0c2fc737083611ac
juniorconde <rubenscondejr@uol.com.br>
2024-07-11 21:41:24 -0300

    estudo

--- lasanha1.rb
+++ lasanha1.rb
@@ -1,3 +1,13 @@
+#Entrada pelo usuário
 puts "Digite o Tempo Necessário, minutos exemplo 10, 20, 30"
-tempo = gets.to_i
-puts " O tempo de Preparo é de #{tempo}"
+Tempo_Esperado_no_Forno = gets.to_i
+puts "O tempo de Preparo é de.: #{Tempo_Esperado_no_Forno}"
+
+#Entrada pelo usuário
+puts "Quantos Minutos a Lazanha Já ficou no Forno"
+Minutos_No_Forno = gets.to_i
+puts "Minutos no Forno.: #{Minutos_No_Forno}"
+
+# Método para Calcular tempo restante de Forno
+Tempo_Restante =  Tempo_Esperado_no_Forno - Minutos_No_Forno
+puts "Tempo Restante no Forno.: #{Tempo_Restante}"
