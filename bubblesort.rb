afa8b3e50935f548aeced8a0ce4fb1cd2d6c5567
juniorconde <rubenscondejr@uol.com.br>
2024-07-17 14:40:03 -0300

    UP

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,7 +1,46 @@
 # vamos criar um array, com entrada do numeros pelo usuários
 
+# Pede ao usuário para digitar o tamanho do array
+puts "Digite o tamanho do array: "
+  tamanho = gets.chomp.to_i
+# Verifica se o tamanho é positivo
+if tamanho > 0
+  # Cria um array vazio
+  numeros = []
+
+  # Loop para coletar os elementos do array
+  tamanho.times do |i|
+    print "Digite o número #{i + 1}: "
+    numero = gets.chomp.to_i
+    numeros << numero
+  end
+
+  # Exibe o array final
+  puts "Você digitou os números: #{numeros.inspect}"
+else
+  puts "Por favor, digite um tamanho de array positivo."
+end
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+=begin
 array = [64, 34, 25, 12, 22, 11, 90]
 n = array.length
 
@@ -20,3 +59,4 @@ loop do
 end
 
 puts "Array ordenado: #{array}"
+=end
