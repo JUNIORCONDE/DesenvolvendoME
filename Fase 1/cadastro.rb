07d32cbbb90aa277fd416d515f9d1c39ec09afa8
juniorconde <rubenscondejr@uol.com.br>
2024-09-11 06:39:15 -0300

    feat(add functionality to register new students)

--- Fase 1/cadastro.rb
+++ Fase 1/cadastro.rb
@@ -1,6 +1,11 @@
 class Aluno
-  attr_acessor :nome, :idade
+  attr_accessor :nome, :idade
   def initialize(nome, idade)
     @nome = nome
     @idade = idade
   end
+
+  def to_s
+    "Nome: #{@nome}, Idade: #{@idade}"
+  end
+end
