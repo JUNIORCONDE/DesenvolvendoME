d164a1d761fee2ac792565cc2be7f71ab312fbf8
juniorconde <rubenscondejr@uol.com.br>
2024-09-12 07:24:16 -0300

    feat(funcionalidade de opção cadastro de alunos)

--- Fase 1/cadastro.rb
+++ Fase 1/cadastro.rb
@@ -25,3 +25,25 @@ class CadastroDeAlunos
       puts "Aluno #{nome} cadastrado com sucesso!"
     end
   end
+
+# Lista todos os alunos
+def listar_alunos
+  if @alunos.empty?
+    puts "Nenhum aluno Casdastrado."
+  else
+    puts "Alunos Cadastrados:"
+    @alunos.each { |aluno| puts aluno }
+  end
+end
+
+# Remove um aluno pelo nome, com feedback mais detalhado
+def remover_aluno(nome)
+  aluno_encontrado = @alunos.find { |aluno| aluno.nome == nome }
+  if aluno_encontrado
+    @alunos.delete(aluno_encontrado)
+    puts "Aluno #{aluno_encontrado.nome}, Idade: #{aluno_encontrado.idade} removido com sucesso."
+  else
+    puts "Aluno #{nome} não encontrado."
+  end
+end
+end
