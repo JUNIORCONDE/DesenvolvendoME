ff07558d427edd4affb498be1c13bc259188dc42
juniorconde <rubenscondejr@uol.com.br>
2024-09-12 06:11:11 -0300

    feat(creat funcionality add new student)s

--- Fase 1/cadastro.rb
+++ Fase 1/cadastro.rb
@@ -9,3 +9,19 @@ class Aluno
     "Nome: #{@nome}, Idade: #{@idade}"
   end
 end
+
+class CadastroDeAlunos
+  def initialize
+    @alunos = []
+  end
+
+  # Adiciona um novo aluno com validação de duplicatas
+  def adicionar_aluno(nome, idade)
+    if @alunos.any? { |aluno| aluno.nome == nome }
+      puts "Aluno com o nome #{nome} já está cadastrado."
+    else
+      aluno = Aluno.new(nome, idade)
+      @alunos << aluno
+      puts "Aluno #{nome} cadastrado com sucesso!"
+    end
+  end
