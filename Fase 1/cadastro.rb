0f10785c17c7d03db2c7747843509b48674d4812
juniorconde <rubenscondejr@uol.com.br>
2024-09-13 06:32:44 -0300

    feat(Funcionalidades Menu, Case, Opções)

--- Fase 1/cadastro.rb
+++ Fase 1/cadastro.rb
@@ -1,5 +1,6 @@
 class Aluno
   attr_accessor :nome, :idade
+
   def initialize(nome, idade)
     @nome = nome
     @idade = idade
@@ -26,24 +27,71 @@ class CadastroDeAlunos
     end
   end
 
-# Lista todos os alunos
-def listar_alunos
-  if @alunos.empty?
-    puts "Nenhum aluno Casdastrado."
-  else
-    puts "Alunos Cadastrados:"
-    @alunos.each { |aluno| puts aluno }
+  # Lista todos os alunos
+  def listar_alunos
+    if @alunos.empty?
+      puts "Nenhum aluno cadastrado."
+    else
+      puts "Alunos cadastrados:"
+      @alunos.each { |aluno| puts aluno }
+    end
+  end
+
+  # Remove um aluno pelo nome, com feedback mais detalhado
+  def remover_aluno(nome)
+    aluno_encontrado = @alunos.find { |aluno| aluno.nome == nome }
+    if aluno_encontrado
+      @alunos.delete(aluno_encontrado)
+      puts "Aluno #{aluno_encontrado.nome}, Idade: #{aluno_encontrado.idade} removido com sucesso."
+    else
+      puts "Aluno #{nome} não encontrado."
+    end
   end
 end
 
-# Remove um aluno pelo nome, com feedback mais detalhado
-def remover_aluno(nome)
-  aluno_encontrado = @alunos.find { |aluno| aluno.nome == nome }
-  if aluno_encontrado
-    @alunos.delete(aluno_encontrado)
-    puts "Aluno #{aluno_encontrado.nome}, Idade: #{aluno_encontrado.idade} removido com sucesso."
-  else
-    puts "Aluno #{nome} não encontrado."
+# Interface do Sistema
+def menu
+  cadastro = CadastroDeAlunos.new
+  loop do
+    puts "\nBem Vindo ao Sistema de Cadastro de Alunos"
+    puts "1. Cadastrar Aluno"
+    puts "2. Listar Alunos"
+    puts "3. Remover Aluno"
+    puts "4. Sair"
+    print "Escolha uma das Opções"
+    opcao = gets.chomp.to_i
+
+    case opcao
+    when 1
+      print "Digite o nome do aluno: "
+      nome = gets.chomp
+      if nome.empty?
+        puts "Nome não pode ser vazio."
+        next
+      end
+
+      print "Digite a idade do aluno: "
+      idade = gets.chomp.to_i
+      if idade <= 0
+        puts "Idade deve ser um número positivo."
+        next
+      end
+      cadastro.adicionar_aluno(nome, idade)
+
+    when 2
+      cadastro.listar_alunos
+    when 3
+      print "Digite o nome do aluno a ser removido: "
+      nome = gets.chomp
+      cadastro.remover_aluno(nome)
+    when 4
+      puts "Saindo do sistema..."
+      break
+    else
+      puts "Opção inválida, tente novamente."
+    end
   end
 end
-end
+
+# Inicializa o menu
+menu
